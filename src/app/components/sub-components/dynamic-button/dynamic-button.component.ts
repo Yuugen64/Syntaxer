dede047cbb7d8a10561bda6b0bed5018ba77de38
yg64Obsidian <yg64Obsidian@proton.me>
2023-09-26 13:49:25 -0500

    [UI | Tinkerer:: Controls] - Finally got the dynamic buttons WITH icons displaying and behaving correctly. Will work on fixing the title font bug next.

--- src/app/components/sub-components/dynamic-button/dynamic-button.component.ts
+++ src/app/components/sub-components/dynamic-button/dynamic-button.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dynamic-button',
   templateUrl: './dynamic-button.component.html',
   styleUrls: ['./dynamic-button.component.css']
 })
-export class DynamicButtonComponent {
+export class DynamicButtonComponent implements OnInit {
+  
+  //For dynamically swapping out the icons on the buttons using a dedicated CSS class in the icon area
+  @Input() iconClass: string = "";
+  
+  constructor(){}
+
+  ngOnInit(): void {}
+
 
 }
